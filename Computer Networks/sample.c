0f291d58f14aa9e3554678c93d0b34122b8cd08c
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-08-03 08:33:24 +0530

    sample

--- Computer Networks/sample.c
+++ Computer Networks/sample.c
@@ -1,3 +1,126 @@
+//daytime server
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+
+int main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    // Create socket
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize server address struct
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    // Bind socket to the specified address and port
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Socket binding failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen for incoming connections
+    if (listen(server_socket, 5) == -1) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("DayTime Server listening on port %d...\n", PORT);
+
+    while (1) {
+        // Accept incoming connection
+        if ((client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len)) == -1) {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Get current time and process ID
+        time_t current_time;
+        time(&current_time);
+        struct tm *timeinfo = localtime(&current_time);
+        pid_t server_pid = getpid();
+
+        // Format time
+        char time_buffer[128];
+        strftime(time_buffer, sizeof(time_buffer), "%Y-%m-%d %H:%M:%S", timeinfo);
+
+        // Prepare response message
+        char response[256];
+        snprintf(response, sizeof(response), "Server PID: %d\nCurrent Time: %s\n", server_pid, time_buffer);
+
+        // Send response to client
+        send(client_socket, response, strlen(response), 0);
+
+        close(client_socket);
+    }
+
+    close(server_socket);
+
+    return 0;
+}
+//daytime client
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 12345
+#define BUFFER_SIZE 256
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+
+    // Create socket
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize server address struct
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to the server
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[BUFFER_SIZE];
+    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+    if (bytes_received == -1) {
+        perror("Receive failed");
+        exit(EXIT_FAILURE);
+    }
+
+    buffer[bytes_received] = '\0';
+    printf("Received from server:\n%s", buffer);
+
+    close(client_socket);
+
+    return 0;
+}
+
+
 //udp matrix rows
 //server
 #include <stdio.h>
