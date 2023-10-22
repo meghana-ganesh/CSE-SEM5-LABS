d482b8ccf1ac6242f5653b1fcec3111d09ba61d1
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-10-22 10:30:58 +0530

    Lab9 Switch

---
+++ Compiler Design/Lab9_switch__jj/input.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+main(){
+	int a,b;
+	char b,c;
+	c = a - b;
+	a = a > b;
+	b = c * c;
+	switch (a)
+	{
+		case 'a':
+			b = 1;
+			break;
+		case 'b':
+			for(i=0;i<5;i = i+1){
+				b = 1;
+			}
+			break;
+		case 3:
+			while(i<n){
+				i = i + 1;
+			}
+			c = 1;
+			break;
+		default:
+			k = 1;
+			break;
+	}
+}
